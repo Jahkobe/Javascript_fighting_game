3d364b7efcbf82efa052b2e5e1336fa794cbd41f
Jahkobe <Jpursell264@gmail.com>
2020-03-28 19:11:16 -0600

    player does first attack

--- app.js
+++ app.js
@@ -1,75 +1,86 @@
-// start screen
-//
-// you start fighting with an opponent
-//
-// need to create a dialouge box with a button that keeps the story moving.
-// get some cool font to do it with
-//
-
 $(() => {
-    
+
+let playerHealthBar = 100;
+let playerMagicBar = 100;
+let enemyHealthBar = 100;
+let enemyMagicBar = 100;
+$('.playerHealthBar').append(playerHealthBar);
+$('.playerMagicBar').append(playerMagicBar);
+$('.enemyHealthBar').append(enemyHealthBar);
+$('.enemyMagicBar').append(enemyMagicBar);
 
 const enemyHealth = () => {
-    let health = 100;
+enemyHealthBar = enemyHealthBar - PlayerStats.hitDamage;
+$('.textBox').text(`${Zombie.name} took ${PlayerStats.hitDamage} Damage!`)
+$('.enemyHealthBar').text(`Health: ${enemyHealthBar}`);
+enemyAttack();
 }
 
 const playerHealth = () => {
-    let health = 100;
-}
-
-const playerAttack = () => {
-$h1 = $('<h1>');
-$('.textBox').text(`${Hero.name} Attacks`);
- console.log(`${Hero.name} Attacks`);
-
-}
-
-const enemyAttack = () => {
     
 }
 
+const playerAttack = () => {
+$('.textBox').text(`${Hero.name} Attacks`);
+console.log(`${Hero.name} Attacks`);
+setTimeout(enemyHealth, 2000);
+}
+
+const enemyAttack = () => {
+
+}
+
 const playerDefense = () => {
-$h1 = $('<h1>');
 $('.textBox').text(`${Hero.name} Defends`);
 console.log(`${Hero.name} Defends`);
 }
 
 class Player {
-    constructor(name, power, health){
-    this.name = name;
-    this.power = power;
-    this.health = health;
-    }
-}
-
-class Enemy {
-    constructor(name, power, health){
+    constructor(name, power, health, defense){
         this.name = name;
         this.power = power;
         this.health = health;
+        this.defense = defense;
         }
-    
 }
 
-const Hero = new Player("Hero", "superStrength", 100);
+class Enemy {
+    constructor(name, power, health, defense){
+        this.name = name;
+        this.power = power;
+        this.health = health;
+        this.defense = defense;
+        }  
+}
+
+const PlayerStats = {
+    till_Next_Level: "500exp",
+    strength_Level: "kinda_Weak",
+    hitDamage: "hitDamage"
+};
+
+const Hero = new Player("Hero", "superStrength", 100, "ok");
 console.log(Hero);
+const Zombie = new Enemy("Zombie", "zombieStrength", 100, "weak");
+console.log(Zombie);
+
+PlayerStats.hitDamage = Math.floor(Math.random() * 10 + 1);
+console.log(PlayerStats.hitDamage);
 
 $('.attack').on('click', playerAttack);
 
 $('.attack').on('click', function(){
-        $('.enemyImage').css('background-color' , 'red');
+        $('.enemyImg').css('background-color' , 'red');
     });
 
 $('.attack').on('click', function(){
-    $('.enemyImage').css('animation-name', 'flip-horizontal-bottom');
+    $('.enemyImg').css('animation-name', 'flip-horizontal-bottom');
     });
-
-    $('.attack').on('click', function(){
-        $('.enemyImg').css('animation-name', 'flip-horizontal-bottom');
-        });
-
-
 
 $('.defense').on('click', playerDefense);
 
+
+
+
+// $ closing tag
 });

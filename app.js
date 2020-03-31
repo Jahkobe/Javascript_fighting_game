3b6d7771ae8b10802664bca1e61bd772e5639f58
Jahkobe <Jpursell264@gmail.com>
2020-03-31 02:09:47 -0600

    little bit more work done

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ $('.enemyHealthBar').append(enemyHealthBar);
 $('.enemyMagicBar').append(enemyMagicBar);
 
 const enemyHealth = () => {
-
+// array used to pick a random number from in order to see how much damage the player inflicts on the enemy
 const superStrength = [0,1,2,3,4,5,6,7,8,9,10]
+// using the array to pick a random number then storing it in a variable
 PlayerStats.hitDamage = superStrength[Math.floor(Math.random() * superStrength.length)];
 enemyHealthBar = enemyHealthBar - PlayerStats.hitDamage;
 $('.textBox').text(`${Zombie.name} took ${PlayerStats.hitDamage} Damage!`)
@@ -21,12 +22,19 @@ setTimeout(enemyAttack, 2000);
 }
 
 const playerHealth = () => {
+// array used to pick a random number from in order to see how much damage the enemy inflicts on the player
 const zombieStrength = [0,1,2,3,4,5,6,7,8,9]
 PlayerStats.hitDamage = zombieStrength[Math.floor(Math.random() * zombieStrength.length)];
 playerHealthBar = playerHealthBar - PlayerStats.hitDamage;
-$('.textBox').text(`${Hero.name} took ${PlayerStats.hitDamage} Damage!`)
-$('.playerHealthBar').text(`Health: ${playerHealthBar}`);
-$('.enemyImg').css('background-color' , 'white');
+
+    if(PlayerStats.hitDamage > 0){
+        $('.textBox').text(`${Hero.name} took ${PlayerStats.hitDamage} Damage!`)
+        $('.playerHealthBar').text(`Health: ${playerHealthBar}`);
+        $('.playerImg').css('background-color' , 'red');
+        $('.playerImg').css('animation-name', 'flip-horizontal-bottom');
+        setTimeout(backToNormalWindow, 2000);       
+    }
+
 }
 
 const playerAttack = () => {
@@ -39,6 +47,10 @@ const enemyAttack = () => {
 $('.textBox').text(`${Zombie.name} Attacks`);
 console.log(`${Zombie.name} Attacks`);
 setTimeout(playerHealth, 2000);
+}
+
+const backToNormalWindow = () => {
+$('.playerImg').css('background-color' , 'white');
 }
 
 const playerDefense = () => {

070ba2fb91c5e9252164702d80f8b8a9658d3e27
Jahkobe <Jpursell264@gmail.com>
2020-03-31 01:42:16 -0600

    Attacking back and forth functions

--- app.js
+++ app.js
@@ -10,15 +10,23 @@ $('.enemyHealthBar').append(enemyHealthBar);
 $('.enemyMagicBar').append(enemyMagicBar);
 
 const enemyHealth = () => {
+
+const superStrength = [0,1,2,3,4,5,6,7,8,9,10]
+PlayerStats.hitDamage = superStrength[Math.floor(Math.random() * superStrength.length)];
 enemyHealthBar = enemyHealthBar - PlayerStats.hitDamage;
 $('.textBox').text(`${Zombie.name} took ${PlayerStats.hitDamage} Damage!`)
 $('.enemyHealthBar').text(`Health: ${enemyHealthBar}`);
 $('.enemyImg').css('background-color' , 'white');
-enemyAttack();
+setTimeout(enemyAttack, 2000);
 }
 
 const playerHealth = () => {
-    
+const zombieStrength = [0,1,2,3,4,5,6,7,8,9]
+PlayerStats.hitDamage = zombieStrength[Math.floor(Math.random() * zombieStrength.length)];
+playerHealthBar = playerHealthBar - PlayerStats.hitDamage;
+$('.textBox').text(`${Hero.name} took ${PlayerStats.hitDamage} Damage!`)
+$('.playerHealthBar').text(`Health: ${playerHealthBar}`);
+$('.enemyImg').css('background-color' , 'white');
 }
 
 const playerAttack = () => {
@@ -28,7 +36,9 @@ setTimeout(enemyHealth, 2000);
 }
 
 const enemyAttack = () => {
-
+$('.textBox').text(`${Zombie.name} Attacks`);
+console.log(`${Zombie.name} Attacks`);
+setTimeout(playerHealth, 2000);
 }
 
 const playerDefense = () => {
@@ -64,9 +74,6 @@ const Hero = new Player("Hero", "superStrength", 100, "ok");
 console.log(Hero);
 const Zombie = new Enemy("Zombie", "zombieStrength", 100, "weak");
 console.log(Zombie);
-
-PlayerStats.hitDamage = Math.floor(Math.random() * 10 + 1);
-console.log(PlayerStats.hitDamage);
 
 $('.attack').on('click', playerAttack);
 
